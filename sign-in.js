74ca914be89d933234fcfd6f409c6bf4793d6dc9
Jack <30997515+jackmercy@users.noreply.github.com>
2018-12-19 15:14:00 +0700

    Update sign-in.js

--- sign-in.js
+++ sign-in.js
@@ -2,21 +2,17 @@
 window.onload = function() {
 // Login form
 var loginBtn = document.getElementById('login-button');
-
 var _emailInput = document.getElementById('email');
-
 var _passwordInput = document.getElementById('password');
 var passwordErrorText = document.getElementById('password-err-text');
-
 var headerText = document.getElementById('header-text');
-
 var notificationMessage = document.getElementById('notification-message');
 var notificationErr = document.getElementById('notification-error');
 var notificationSuccess = document.getElementById('notification-success');
 
+
 var signInContainer = document.getElementById('sign-in-container');
 var forgotPasswordContainer = document.getElementById('forgot-password-container');
-
 forgotPasswordContainer.style.display = 'none';
 
 // validator 8 chars: .{8,} expr: ^(?=[a-zA-Z0-9#@$?]{8,}$)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).*
@@ -24,7 +20,7 @@ function validateEmail(email) {
     var expr = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
     return expr.test(String(email).toLowerCase());
 }
-// Validator
+var databaseConnection = 'Username-Password-Authentication';
 var pageTexts = {
     TITLE: {
         SIGN_IN: '',
@@ -41,34 +37,6 @@ var pageTexts = {
         FORGOT_NOTIFICATION_INTRO: '',
     }
 };
-
-function _signInViewModel() {
-    var self = this;
-
-    // Email
-    self.emailInput = ko.observable('');
-    self.showMandatoryEmailErrMessage = ko.observable(false),
-    self.isEmailValidated = ko.observable(false),
-    // Password
-    self.passwordInput = ko.observable(''),
-    self.showMandatoryPasswordErrMessage = ko.observable(false),
-    self.isPasswordValidated = ko.observable(false),
-    // Forgot email
-    self.forgotEmailInput = ko.observable(''),
-    self.showMandatoryForgotEmailErrMessage = ko.observable(false),
-    self.isForgotEmailValidated = ko.observable(false),
-
-    // page texts
-    self.pageTexts = ko.observable();
-
-    if (config.extraParams.ui_locales === "de") {
-        self.pageTexts(pageTexts_de);
-    } else {
-        // default language
-        self.pageTexts(pageTexts_en);
-    }
-};
-
 var pageTexts_en = {
     TITLE: {
         SIGN_IN: 'SIGN IN',
@@ -101,82 +69,94 @@ var pageTexts_de = {
         FORGOT_NOTIFICATION_INTRO: 'Please enter your email address. We will send you an email to reset your password.',
     }
 };
+function _signInViewModel() {
+    var self = this;
 
+    // Email
+    self.emailInput = ko.observable('');
+    self.showMandatoryEmailErrMessage = ko.observable(false),
+    self.isEmailValidated = ko.observable(false),
+    // Password
+    self.passwordInput = ko.observable(''),
+    self.showMandatoryPasswordErrMessage = ko.observable(false),
+    self.isPasswordValidated = ko.observable(false),
+    // Forgot email
+    self.forgotEmailInput = ko.observable(''),
+    self.showMandatoryForgotEmailErrMessage = ko.observable(false),
+    self.isForgotEmailValidated = ko.observable(false),
 
+    // page texts
+    self.pageTexts = ko.observable();
 
-function onChangeEmailValue(input, showMandatoryErrMessage, isValidated, errElement) {
-    var emailErrorText = document.getElementById(errElement);
-    if (input().length === 0) {
-        showMandatoryErrMessage(true);
-        isValidated(false);
-        return;
-    } else if (!validateEmail(input())) {
-        showMandatoryErrMessage(false);
-        isValidated(false);
-        emailErrorText.innerHTML = pageTexts.ERROR.INVALID_EMAIL;
-    } else if (validateEmail(input())) {
-        emailErrorText.innerHTML = '';
-        showMandatoryErrMessage(false);
-        isValidated(true);
-    }
-}
-
-// simple: > 8 chars
-function onChangePasswordValue(input, showMandatoryErrMessage, isValidated) {
-    if (input().length === 0) {
-        showMandatoryErrMessage(true);
-        return;
-    } else if(input().length > 0 && input().length < 8) {
-        showMandatoryErrMessage(false);
-        isValidated(false);
-        passwordErrorText.innerHTML = pageTexts.ERROR.PASSWORD_LENGTH;
-    } else {
-        showMandatoryErrMessage(false);
-        isValidated(true);
-        passwordErrorText.innerHTML = '';
-    }
-}
-
-
-
-// Auth0
-
-var databaseConnection = 'Username-Password-Authentication';
-
-// login logic
-
-function signInClicked(e) {
-    e.preventDefault();
-    var email = emailInput();
-    var password = passwordInput();
-
-    webAuth.login({
-        realm: databaseConnection,
-        username: email,
-        password: password
-    }, function(err) {
-        if (err) displayError(err);
-    });
-}
-
-function forgotPassword(e) {
-    e.preventDefault();
-    var _email = forgotEmailInput();
-
-    webAuth.changePassword({
-    connection: databaseConnection,
-    email: _email
-    }, function(err, resp) {
-        if (err) displayError(err);
-        else {
-            // back to sign in page
-            forgotPasswordContainer.style.display = 'none';
-            signInContainer.style.display = 'block';
-            displaySuccess(resp);
-            headerText.innerHTML = pageTexts.TITLE.SIGN_IN;
-        }
+    this.signInClicked = function() {
+        event.preventDefault();
+        var email = self.emailInput();
+        var password = self.passwordInput();
+    
+        webAuth.login({
+            realm: databaseConnection,
+            username: email,
+            password: password
+        }, function(err) {
+            if (err) displayError(err);
+        });
+    };
+    this.forgotPassword = function() {
+        event.preventDefault();
+        var _email = self.forgotEmailInput();
+    
+        webAuth.changePassword({
+        connection: databaseConnection,
+        email: _email
+        }, function(err, resp) {
+            if (err) displayError(err);
+            else {
+                // back to sign in page
+                forgotPasswordContainer.style.display = 'none';
+                signInContainer.style.display = 'block';
+                displaySuccess(resp);
+                headerText.innerHTML = pageTexts.TITLE.SIGN_IN;
+            }
         });
     }
+
+    this.onChangeEmailValue = function(input, showMandatoryErrMessage, isValidated, errElement) {
+        var emailErrorText = document.getElementById(errElement);
+        if (input().length === 0) {
+            showMandatoryErrMessage(true);
+            isValidated(false);
+            return;
+        } else if (!validateEmail(input())) {
+            showMandatoryErrMessage(false);
+            isValidated(false);
+            emailErrorText.innerHTML = pageTexts.ERROR.INVALID_EMAIL;
+        } else if (validateEmail(input())) {
+            emailErrorText.innerHTML = '';
+            showMandatoryErrMessage(false);
+            isValidated(true);
+        }
+    }
+    this.onChangePasswordValue = function(input, showMandatoryErrMessage, isValidated) {
+        if (input().length === 0) {
+            showMandatoryErrMessage(true);
+            return;
+        } else if(input().length > 0 && input().length < 8) {
+            showMandatoryErrMessage(false);
+            isValidated(false);
+            passwordErrorText.innerHTML = pageTexts.ERROR.PASSWORD_LENGTH;
+        } else {
+            showMandatoryErrMessage(false);
+            isValidated(true);
+            passwordErrorText.innerHTML = '';
+        }
+    }
+    /* if (config.extraParams.ui_locales === "de") {
+        self.pageTexts(pageTexts_de);
+    } else {
+        // default language
+        self.pageTexts(pageTexts_en);
+    } */
+};
 
 function displaySuccess(message) {
     notificationMessage.innerHTML = '';
@@ -203,10 +183,6 @@ function onForgotPasswordLinkClicked() {
     notificationErr.innerHTML = '';
 }
 
-
-
-document.getElementById('sign-in-button').addEventListener('click', signInClicked);
-document.getElementById('forgot-password-button').addEventListener('click', forgotPassword);
 
 ko.applyBindings(new _signInViewModel());
 
